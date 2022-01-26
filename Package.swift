9434da1dc177ffac9bbc3f197a6bdbca33c2ced8
Michaël Blin <mblin@figarocms.fr>
2022-01-26 23:44:02 +0100

    remove tests

--- Package.swift
+++ Package.swift
@@ -25,9 +25,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MB_Button",
-            dependencies: ["SnapKit"]),
-        .testTarget(
-            name: "MB_ButtonTests",
-            dependencies: ["MB_Button"]),
+            dependencies: ["SnapKit"])
     ]
 )
